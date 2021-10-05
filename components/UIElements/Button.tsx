8ae50f678844ee3143f4cc97973e03def935d08a
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-05 20:34:11 +0200

    Update Button.tsx
    How could I forget onClick...

--- components/UIElements/Button.tsx
+++ components/UIElements/Button.tsx
@@ -2,7 +2,10 @@ import { ButtonHTMLAttributes } from 'react';
 
 const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
   return (
-    <button className="text-white w-max px-2 py-1 shadow-sm hover:shadow-md text-xl rounded-md bg-indigo-600 hover:bg-indigo-700 border-2">
+    <button
+      onClick={props.onClick}
+      className="text-white w-max px-2 py-1 shadow-sm hover:shadow-md text-xl rounded-md bg-indigo-600 hover:bg-indigo-700 border-2"
+    >
       {props.children}
     </button>
   );
