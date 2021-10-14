755734451c697b3609a759cccdc45f47ef892e14
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-14 23:11:15 +0200

    Wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        sidebar: '90vh',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: { backgroundColor: ['active'], height: ['hover'] },
   },
   plugins: [],
 };
