f237fbd29fdfa4e93c89695cfd528c616e3fa097
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-15 16:01:00 +0200

    Create solutionsArr.ts

---
+++ data/solutionsArr.ts
@@ -0,0 +1,72 @@
+import { ISolution } from '../interfaces';
+import descStrings from '../descriptions/descStrings.json';
+import codeString from '../descriptions/codeStrings.json';
+
+const solutionsArr = <ISolution[]>[
+  {
+    id: 0,
+    title: '',
+    hint: '',
+    desc: descStrings.desc0,
+    code: codeString.solution0,
+    solutionUrl: '',
+  },
+  {
+    id: 1,
+    title: '',
+    hint: '',
+    desc: descStrings.desc1,
+    code: codeString.solution1,
+    solutionUrl: '',
+  },
+  {
+    id: 2,
+    title: '',
+    hint: '',
+    desc: descStrings.desc2,
+    code: codeString.solution2,
+    solutionUrl: '',
+  },
+  {
+    id: 3,
+    title: '',
+    hint: '',
+    desc: descStrings.desc3,
+    code: codeString.solution3,
+    solutionUrl: '',
+  },
+  {
+    id: 4,
+    title: '',
+    hint: '',
+    desc: descStrings.desc4,
+    code: codeString.solution4,
+    solutionUrl: '',
+  },
+  {
+    id: 5,
+    title: '',
+    hint: '',
+    desc: descStrings.desc5,
+    code: codeString.solution5,
+    solutionUrl: '',
+  },
+  {
+    id: 6,
+    title: '',
+    hint: '',
+    desc: descStrings.desc6,
+    code: codeString.solution6,
+    solutionUrl: '',
+  },
+  {
+    id: 7,
+    title: '',
+    hint: '',
+    desc: descStrings.desc7,
+    code: codeString.solution7,
+    solutionUrl: '',
+  },
+];
+
+export default solutionsArr;
