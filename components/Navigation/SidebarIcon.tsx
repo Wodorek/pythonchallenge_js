d99aacbf1ec0864d133d6c5e653a2d304a9d8be0
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-15 19:59:47 +0200

    Components split

---
+++ components/Navigation/SidebarIcon.tsx
@@ -0,0 +1,20 @@
+import HamburgerIcon from '../../icons/hamburger.svg';
+import CloseIcon from '../../icons/close.svg';
+
+interface IProps {
+  open: () => void;
+  isOpen: boolean;
+}
+
+const SidebarIcon: React.FC<IProps> = (props) => {
+  return (
+    <button
+      onClick={props.open}
+      className="xl:hidden fill-current text-white bg-gray-900 z-20 fixed bottom-10 right-10 h-20 w-20 flex items-center justify-center rounded-full"
+    >
+      {props.isOpen ? <CloseIcon /> : <HamburgerIcon />}
+    </button>
+  );
+};
+
+export default SidebarIcon;
