d4ff60475996781c411ea8a37b2de3895ac59297
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-26 19:49:31 +0200

    Create Solution12.tsx

---
+++ lib/solution12/Solution12.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Solution12 = () => {
+  useEffect(() => {
+    fetch('./api/solutions/forSolution12');
+  });
+
+  return <div></div>;
+};
+
+export default Solution12;
