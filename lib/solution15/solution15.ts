ecb46b446ce22343d33ce0b52496e7fa3fcadae9
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-10-28 18:20:41 +0200

    Create solution15.ts

---
+++ lib/solution15/solution15.ts
@@ -0,0 +1,20 @@
+const solution15 = () => {
+  //we know the year is between 1006 and 1996
+  //first leap year in this range is 1016.
+
+  //knowing that, loop over the range, checking for years that have Jan 26th as monday
+
+  const mondays = [];
+
+  //i+=20 since leap years ending in 6 come every 20 years
+  for (let i = 1036; i < 1996; i += 20) {
+    if (new Date(`${i}-1-26`).getDay() === 1) {
+      mondays.push(i);
+    }
+  }
+
+  //log second to last monday, aka "second younges"
+  console.log(mondays[mondays.length - 2]);
+};
+
+export default solution15;
